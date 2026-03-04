4407ca2b970403e70b7e840b0b989cf1d4ff45cb
Javier Montero <monterodotjavier@gmail.com>
2026-03-03 23:48:02 -0500

    created yaml file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/edwinaguila/',
   plugins: [react()],
+  base: "/edwinaguila/", // Need this for GitHub Pages since it's hosted at /edwinaguila/
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
